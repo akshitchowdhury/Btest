99f01856dc80ce160cec0e8e8f59ce6bcc3fae5f
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-08-16 15:35:27 +0530

    fourth imp

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-import exp from 'constants'
+import bodyParser from 'body-parser'
 import express from 'express'
 import moduleName from 'module'
 
 const app = express()
+const port = 3000
+
+app.listen(port, ()=>{
+    console.log("listening to port "+ port)
+})
+
+
+
