f4f16ec8c92549124eee726dd9e7f0992ff1d885
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-08-16 15:42:09 +0530

    final imp

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import moduleName from 'module'
+import {dirname} from 'path'
+import fileURLToPath from 'url'
 
 const app = express()
 const port = 3000
+const _dirname = dirname(fileURLToPath(import.meta.url))
+
 
 app.listen(port, ()=>{
     console.log("listening to port "+ port)
 })
 
+app.get("/", (rew,res)=>{
 
+    res.sendFile(_dirname, "/index.html")
+})
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post("/submit", (req,res)=>{
+    console.log(req.body)
+})
 
